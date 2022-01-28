e2cc1fcf7d149ba78f2b31f2501034173c29c5cf
Temitope <kingpeace12345@gmail.com>
2022-01-28 10:45:01 +0100

    Me try again and harder

--- project/settings.py
+++ project/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 template_dir= BASE_DIR / "templates"
@@ -200,3 +201,5 @@ MESSAGE_STORAGE = 'django.contrib.messages.storage.cookie.CookieStorage'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
